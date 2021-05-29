2b63c9c7467c8117e0a96de17a028718655d4122
german77 <juangerman-13@hotmail.com>
2021-05-29 16:07:58 -0500

    Use SDL

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -22,137 +22,192 @@ std::vector<u8> program{ 0x20, 0x06, 0x06, 0x20, 0x38, 0x06, 0x20, 0x0d, 0x06, 0
 	0xa6, 0x03, 0xa9, 0x00, 0x81, 0x10, 0xa2, 0x00, 0xa9, 0x01, 0x81, 0x10, 0x60, 0xa2, 0x00, 0xea,
 	0xea, 0xca, 0xd0, 0xfb, 0x60 };
 
-int main() {
+int main(int argc, char* argv[]) {
+	InitializeSDL();
 	cpu.LoadProgram(program);
 	cpu.Reset();
 	while (cpu.IsRunning()) {
-		//cpu.PrintStatus();
-		const CommandParameters parameter = GetCommandParameters();
-		bool next = true;
-		cpu.IncrementCounter(1);
-		switch (parameter.function) {
-		case Function::BRK:
-			cpu.BRK(parameter.mode);
-			break;
-		case Function::JSR:
-			next = cpu.JSR(parameter.mode);
-			break;
-		case Function::RTS:
-			next = cpu.RTS(parameter.mode);
-			break;
-		case Function::JMP:
-			next = cpu.JMP(parameter.mode);
-			break;
-		case Function::BCC:
-			next = cpu.BCC(parameter.mode);
-			break;
-		case Function::BCS:
-			next = cpu.BCS(parameter.mode);
-			break;
-		case Function::BEQ:
-			next = cpu.BEQ(parameter.mode);
-			break;
-		case Function::BMI:
-			next = cpu.BMI(parameter.mode);
-			break;
-		case Function::BNE:
-			next = cpu.BNE(parameter.mode);
-			break;
-		case Function::BPL:
-			next = cpu.BPL(parameter.mode);
-			break;
-		case Function::BVC:
-			next = cpu.BVC(parameter.mode);
-			break;
-		case Function::BVS:
-			next = cpu.BVS(parameter.mode);
-			break;
-		case Function::CLC:
-			cpu.CLC(parameter.mode);
-			break;
-		case Function::CLD:
-			cpu.CLD(parameter.mode);
-			break;
-		case Function::CLI:
-			cpu.CLI(parameter.mode);
-			break;
-		case Function::CLV:
-			cpu.CLV(parameter.mode);
-			break;
-		case Function::TXA:
-			cpu.TXA(parameter.mode);
-			break;
-		case Function::TAX:
-			cpu.TAX(parameter.mode);
-			break;
-		case Function::PHA:
-			cpu.PHA(parameter.mode);
-			break;
-		case Function::PLA:
-			cpu.PLA(parameter.mode);
-			break;
-		case Function::BIT:
-			cpu.BIT(parameter.mode);
-			break;
-		case Function::LSR:
-			cpu.LSR(parameter.mode);
-			break;
-		case Function::ADC:
-			cpu.ADC(parameter.mode);
-			break;
-		case Function::LDA:
-			cpu.LDA(parameter.mode);
-			break;
-		case Function::LDX:
-			cpu.LDX(parameter.mode);
-			break;
-		case Function::LDY:
-			cpu.LDY(parameter.mode);
-			break;
-		case Function::STA:
-			cpu.STA(parameter.mode);
-			break;
-		case Function::DEX:
-			cpu.DEX(parameter.mode);
-			break;
-		case Function::DEY:
-			cpu.DEY(parameter.mode);
-			break;
-		case Function::INC:
-			cpu.INX(parameter.mode);
-			break;
-		case Function::INX:
-			cpu.INX(parameter.mode);
-			break;
-		case Function::INY:
-			cpu.INY(parameter.mode);
-			break;
-		case Function::AND:
-			cpu.AND(parameter.mode);
-			break;
-		case Function::CMP:
-			cpu.CMP(parameter.mode);
-			break;
-		case Function::CPX:
-			cpu.CPX(parameter.mode);
-			break;
-		case Function::CPY:
-			cpu.CPY(parameter.mode);
-			break;
-		case Function::NOP:
-			std::printf("NOP\n");
-			break;
-		default:
-			std::printf("Operation %02x not implemented\n", static_cast<u8>(parameter.opcode));
-			break;
-		}
-		if (next) {
-			cpu.IncrementCounter(parameter.parameters);
-		}
-		
-
+		ExcecuteCode(); 
+		DrawWindow();
+		SDLEvents();
+		SDL_Delay(1);
 	}
+
+	// Destroy the window. This will also destroy the surface
+	SDL_DestroyWindow(window);
+
+	// Quit SDL
+	SDL_Quit();
 	return 0;
+}
+
+void SDLEvents() {
+	SDL_Event e;
+	while (SDL_PollEvent(&e) != 0) {
+		switch (e.type) {
+		case SDL_QUIT:
+			exit(0);
+		case SDL_KEYDOWN:
+			std::printf("KEY DOWn\n");
+			break;
+		case SDL_KEYUP:
+			std::printf("KEY UP\n");
+			break;
+		}
+	}
+}
+
+void DrawWindow() {
+	u16 addr = 0x0200;
+	for (int x = 0; x < 32; x++) {
+		for (int y = 0; y < 32; y++) {
+			u8 pixel = cpu.ReadMemory(addr++);
+			int r, g, b;
+			std::tie(r,g,b) = GetColor(pixel);
+			SDL_SetRenderDrawColor(renderer, r, g, b, 255); 
+			SDL_Rect pix = { x * 20, y * 20,20,20 };
+			SDL_RenderFillRect(renderer, &pix);
+		}
+	}
+	SDL_RenderPresent(renderer);
+}
+std::tuple<u8, u8, u8> GetColor(u8 color) {
+	switch (color) {
+	case 0:return { 0,0,0 };
+	case 1:return { 255,255,255 };
+	case 2:return { 128,128,128 };
+	case 3:return { 255,0,0 };
+	case 4:return { 0,255,0 };
+	case 5:return { 0,0,255 };
+	case 6:return { 255,0,255 };
+	case 7:return { 255,255,0 };
+	default: return { 0,255,255 };
+	}
+}
+void ExcecuteCode() {
+	//cpu.PrintStatus();
+	const CommandParameters parameter = GetCommandParameters();
+	bool next = true;
+	cpu.IncrementCounter(1);
+	switch (parameter.function) {
+	case Function::BRK:
+		cpu.BRK(parameter.mode);
+		break;
+	case Function::JSR:
+		next = cpu.JSR(parameter.mode);
+		break;
+	case Function::RTS:
+		next = cpu.RTS(parameter.mode);
+		break;
+	case Function::JMP:
+		next = cpu.JMP(parameter.mode);
+		break;
+	case Function::BCC:
+		next = cpu.BCC(parameter.mode);
+		break;
+	case Function::BCS:
+		next = cpu.BCS(parameter.mode);
+		break;
+	case Function::BEQ:
+		next = cpu.BEQ(parameter.mode);
+		break;
+	case Function::BMI:
+		next = cpu.BMI(parameter.mode);
+		break;
+	case Function::BNE:
+		next = cpu.BNE(parameter.mode);
+		break;
+	case Function::BPL:
+		next = cpu.BPL(parameter.mode);
+		break;
+	case Function::BVC:
+		next = cpu.BVC(parameter.mode);
+		break;
+	case Function::BVS:
+		next = cpu.BVS(parameter.mode);
+		break;
+	case Function::CLC:
+		cpu.CLC(parameter.mode);
+		break;
+	case Function::CLD:
+		cpu.CLD(parameter.mode);
+		break;
+	case Function::CLI:
+		cpu.CLI(parameter.mode);
+		break;
+	case Function::CLV:
+		cpu.CLV(parameter.mode);
+		break;
+	case Function::TXA:
+		cpu.TXA(parameter.mode);
+		break;
+	case Function::TAX:
+		cpu.TAX(parameter.mode);
+		break;
+	case Function::PHA:
+		cpu.PHA(parameter.mode);
+		break;
+	case Function::PLA:
+		cpu.PLA(parameter.mode);
+		break;
+	case Function::BIT:
+		cpu.BIT(parameter.mode);
+		break;
+	case Function::LSR:
+		cpu.LSR(parameter.mode);
+		break;
+	case Function::ADC:
+		cpu.ADC(parameter.mode);
+		break;
+	case Function::LDA:
+		cpu.LDA(parameter.mode);
+		break;
+	case Function::LDX:
+		cpu.LDX(parameter.mode);
+		break;
+	case Function::LDY:
+		cpu.LDY(parameter.mode);
+		break;
+	case Function::STA:
+		cpu.STA(parameter.mode);
+		break;
+	case Function::DEX:
+		cpu.DEX(parameter.mode);
+		break;
+	case Function::DEY:
+		cpu.DEY(parameter.mode);
+		break;
+	case Function::INC:
+		cpu.INX(parameter.mode);
+		break;
+	case Function::INX:
+		cpu.INX(parameter.mode);
+		break;
+	case Function::INY:
+		cpu.INY(parameter.mode);
+		break;
+	case Function::AND:
+		cpu.AND(parameter.mode);
+		break;
+	case Function::CMP:
+		cpu.CMP(parameter.mode);
+		break;
+	case Function::CPX:
+		cpu.CPX(parameter.mode);
+		break;
+	case Function::CPY:
+		cpu.CPY(parameter.mode);
+		break;
+	case Function::NOP:
+		std::printf("NOP\n");
+		break;
+	default:
+		std::printf("Operation %02x not implemented\n", static_cast<u8>(parameter.opcode));
+		break;
+	}
+	if (next) {
+		cpu.IncrementCounter(parameter.parameters);
+	}
 }
 
 const CommandParameters GetCommandParameters() {
@@ -167,4 +222,31 @@ const CommandParameters GetCommandParameters() {
 	}
 
 	return *it;
+}
+
+void InitializeSDL() {
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::printf("Error initializing SDL\n");
+		system("pause");
+	}
+
+	// Create our window
+	window = SDL_CreateWindow("NEStheEMU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 640, SDL_WINDOW_SHOWN);
+
+	// Make sure creating the window succeeded
+	if (!window) {
+		std::printf("Error creating window\n");
+		system("pause");
+	}
+
+	int result = SDL_CreateWindowAndRenderer(640, 640, NULL, &window, &renderer);
+
+	// Make sure getting the surface succeeded
+	if (result != 0) {
+		std::printf("Error getting renderer\n");
+		system("pause");
+	}
+	// Update the window display
+	SDL_UpdateWindowSurface(window);
 }
--- src/main/main.h
+++ src/main/main.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <vector>
+#include <SDL.h>
 #include "../core/cpu.h"
 #include "../core/commands.h"
 #include "../common/common_types.h"
 
 const CommandParameters GetCommandParameters();
+void InitializeSDL();
+void ExcecuteCode();
+void SDLEvents();
+void DrawWindow();
+std::tuple<u8,u8,u8> GetColor(u8 color);
 
 CPU cpu{};
+
+SDL_Renderer* renderer;
+SDL_Window* window = NULL;
